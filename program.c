9ca9a225ccb8a8f524b44d76775890c80b83117c
WhitetailAni <davidmichael2006@gmail.com>
2023-03-14 21:50:22 -0500

    support for invoking in a script

--- program.c
+++ program.c
@@ -6,9 +6,34 @@
 #include <signal.h>
 #include <string.h>
 #include <ftdi.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-int lightOn (int led);
-int ledNumber = 0;
+bool isInteger(char* str) {
+    // Check if the string is empty or null
+    if (str == NULL || *str == '\0') {
+        return false;
+    }
+    
+    // Check if the string starts with a minus sign
+    if (*str == '-') {
+        ++str;
+    }
+    
+    // Loop through the remaining characters
+    while (*str != '\0') {
+        // Check if the character is not a digit
+        if (!isdigit(*str)) {
+            return false;
+        }
+        ++str;
+    }
+    
+    // All characters are digits, so the string is an integer
+    return true;
+}
+
 
 int lightOn (int led){
     struct ftdi_context *ftdi;
@@ -62,31 +87,33 @@ int lightOn (int led){
     return 0;
 }
 
-int main()
-{
-    printf("Program to interface with the lights on the Alex DCSD cables.\nBased on @matteyeux's DCSD_STATUS project.\nUpdated fork of geosn0w's thing, without the useless christmas light pattern.\n\n");
-    printf("0 = all LEDs off\n1 = green LED\n2 = yellow LED\n3 = red LED\n4 = all on\n5 = green and yellow LEDs\n7 = yellow and green LEDs\n10 = red and green LEDs\n\n");
+int main(int argc, char **argv){
+	if (argc > 3){
+		printf("Invalid arguments");
+    } else if (argc == 2){
+        if(isInteger(argv[1])){
+            lightOn(atoi(argv[1]));
+        }
+        else if(strcmp(argv[1], "--help") == 0){
+            printf("\nProgram to interface with the lights on the Alex DCSD cables.\nBased on @matteyeux's DCSD_STATUS project.\nUpdated fork of geosn0w's thing, without the useless christmas light pattern.\n\n");
+            printf("   To enable LEDs in a script, simply pass the integer value that you would enter. Example: 'dcsd_lights 2' will turn on the yellow LED.\n");
+            printf("   You can also have them enabled for a set amount of time - just add the amount of time in seconds you want to sleep after. Example: 'dcsd_lights 2 5' will turn on the yellow LED, and then turn it off after 5 seconds.\n");
+            printf("   A key for what number corresponds to which light is below:\n\n");
+            printf("0 = all LEDs off\n1 = green LED\n2 = yellow LED\n3 = red LED\n4 = all on\n5+ = any combo, could be one, two, or three LEDs. Changes every time the cable is unplugged and replugged. The program segfaults if you input a number higher than 277\n");
+        }
+	} else if (argc == 3){
+		lightOn(atoi(argv[1]));
+        sleep(atoi(argv[2]));
+        lightOn(0);
+	} else {
+    int ledNumber = 0;
+    
+	printf("\nProgram to interface with the lights on the Alex DCSD cables.\nBased on @matteyeux's DCSD_STATUS project.\nUpdated fork of geosn0w's thing, without the useless christmas light pattern.\nSee --help for more details.\n\n");
+    printf("0 = all LEDs off\n1 = green LED\n2 = yellow LED\n3 = red LED\n4 = all on\n5+ = any combo, could be one, two, or three LEDs. Changes every time the cable is unplugged and replugged. The program segfaults if you input a number higher than 277\n\n");
     do {
         lightOn(ledNumber);
-        printf("Turn on: ");
-        
-        /*
-        0 = all LEDs off
-        1 = green LED
-        2 = yellow LED
-        3 = red LED
-        4 = all LEDs
-        5 = yellow and green LEDs
-        6 = red LED
-        7 = yellow and green LEDs
-        8 = red and yellow LEDs
-        9 = red LED
-        10 = red and green LEDs
-        11 = yellow and green LEDs
-        12+ = all LEDs
-         */
-        
+        printf("> ");
     } while (scanf("%d", &ledNumber)); //Get user input
-    
-	return 0;
+        return 0;
+    }
 }
